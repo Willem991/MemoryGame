b9fd6391163373bde750c48b7c359973035300d0
Willem Ruan <ruan.nieu@gmail.com>
2021-09-21 16:07:00 +0200

    Game win and restart implemented

--- src/App.js
+++ src/App.js
@@ -23,27 +23,32 @@ const App = () => {
   const [menuDisplay, setMenuDisplay] = useState(true);
   const [score, setScore] = useState(0);
   const [buttonClick, setButtonClick] = useState(false);
+  const [name, setName] = useState('');
+  const [memory, setMemory] = useState([]);
 
-  const changeButton = () => {
+  const changeState = (aName) => {
     setButtonClick(!buttonClick);
+    setName(aName);
   };
 
-  const [displayArray, setDisplayArray] = useState([<Card changeButton = {changeButton} key={uniqid()} name = 'Cra' imgsrc = {Cra}></Card>, 
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Iop' imgsrc = {Iop}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Ecaflip' imgsrc = {Ecaflip}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Eliotrope' imgsrc = {Eliotrope}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Eniripsa' imgsrc = {Eniripsa}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Enutrof' imgsrc = {Enutrof}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Feca' imgsrc = {Feca}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Foggernaut' imgsrc = {Foggernaut}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Huppermage' imgsrc = {Huppermage}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Osamodas' imgsrc = {Osamodas}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Sacrier' imgsrc = {Sacrier}></Card>,
-                                                    <Card changeButton = {changeButton} key={uniqid()} name = 'Sram' imgsrc = {Sram}></Card>, 
+  const [displayArray, setDisplayArray] = useState([<Card changeState = {changeState} key={uniqid()} name = 'Cra' imgsrc = {Cra}></Card>, 
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Iop' imgsrc = {Iop}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Ecaflip' imgsrc = {Ecaflip}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Eliotrope' imgsrc = {Eliotrope}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Eniripsa' imgsrc = {Eniripsa}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Enutrof' imgsrc = {Enutrof}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Feca' imgsrc = {Feca}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Foggernaut' imgsrc = {Foggernaut}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Huppermage' imgsrc = {Huppermage}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Osamodas' imgsrc = {Osamodas}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Sacrier' imgsrc = {Sacrier}></Card>,
+                                                    <Card changeState = {changeState} key={uniqid()} name = 'Sram' imgsrc = {Sram}></Card>, 
   ]);
 
   const initializeGame = () => {
     setMenuDisplay(false);
+    setMemory([]);
+    setName('');
   };
 
   const shuffleArray = () => {
@@ -67,23 +72,34 @@ const App = () => {
     setScore(score + 1);
   };
 
-  const updateScoreandArray = () => {
-    shuffleArray();
-    updateScore();
-    console.log("I am running");
-  };
+  let check = true;
 
   useEffect(() => {
     if(buttonClick){
-      shuffleArray();
-      updateScore();
-      setButtonClick(false);
+      console.log(check);
+  
+        for (let index = 0; index < memory.length; index++) {
+          if(name === memory[index]){
+            check = false;
+          };
+        };
+
+      if(check){
+        let mem = memory;
+        mem.push(name);
+        setMemory(mem);
+        console.log(memory);
+        shuffleArray();
+        updateScore();
+        setButtonClick(false);
+      }else{
+        setMenuDisplay(true);
+      }
     }
   });
 
   return(
     <div>
-      {console.log(buttonClick)}
       <TopHeading></TopHeading>
       {(menuDisplay) ? <MainMenu startGame = {initializeGame}></MainMenu> : <Game CharacterArray = {displayArray}></Game>}
     </div>
--- src/Components/card.js
+++ src/Components/card.js
@@ -3,8 +3,12 @@ import './comp.css'
 
 const Card = (props) => {
 
+    const changeTheState = () => {
+        props.changeState(props.name);
+    };
+
     return(
-        <div id = 'card' onClick = {props.changeButton}>
+        <div id = 'card' onClick = {changeTheState}>
             <img alt = 'Dofus Character' src = {props.imgsrc}></img>
             <p>{props.name}</p>
         </div>
